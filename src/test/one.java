d47c88c554db0f00e45a8a8f74d737e848bf80d9
ritam14 <ritamsingha1992@gmail.com>
2021-04-01 22:53:36 +0530

    first commit

---
+++ src/test/one.java
@@ -0,0 +1,17 @@
+package test;
+
+public class one {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n = 5, fact = 1;
+		for(int i = 1; i<=n; i++)
+		{
+			fact = fact * i;
+			System.out.println("Factorial of "+ i + " is :"+fact);
+		}
+
+	}
+
+}
